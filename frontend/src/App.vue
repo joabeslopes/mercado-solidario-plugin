f2269e148ce2356996982a0b4729a683b359ec69
joabeslopes <joabeslopes.jsl@gmail.com>
2025-05-11 21:42:08 -0300

    adding style to router navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,5 +19,26 @@
   align-items: center;
   display: flex;
   flex-direction: column;
+  width: 100%;
+}
+
+nav{
+  background-image: linear-gradient(to bottom right, purple, #f047f0);
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  border-radius: 12px;
+  box-sizing: border-box;
+}
+
+a{
+  color: yellow;
+  padding: 10px;
+  font-size: medium;
+}
+
+a:focus{
+  box-shadow: none;
 }
 </style>
