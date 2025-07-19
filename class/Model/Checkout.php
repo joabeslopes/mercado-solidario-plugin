d61c3d9be2b577d7454f0d3f434fbb19082c57c7
joabeslopes <joabeslopes.jsl@gmail.com>
2025-07-19 18:13:21 -0300

    post_cart funcionando

--- class/Model/Checkout.php
+++ class/Model/Checkout.php
@@ -4,6 +4,7 @@ namespace Mercado_Solidario\Model;
 use Mercado_Solidario\REST;
 use WC_Product_Query;
 use WP_REST_Request;
+use WC_Order;
 
 // don't call the file directly
 defined( 'ABSPATH' ) || die;
@@ -57,6 +58,9 @@ class Checkout {
         $status = 200;
         $messages = [];
 
+        $order = new WC_Order();
+        $user = wp_get_current_user();
+
         $cart = $request['cart'];
 
         if (!$cart) {
@@ -68,13 +72,18 @@ class Checkout {
                     'sku' => $cartProd['sku'],
                     'limit' => 1
                 ];
-
                 $query = new WC_Product_Query($args);
-                $product = $query->get_products()[0];
 
-                if ($product){
+                $result = $query->get_products();
+
+                if ($result){
+                    $product = $result[0];
+
                     $quantity = $product->get_stock_quantity() - $cartProd['quantity'];
-                    if ($quantity < 0) {
+
+                    if ($quantity >= 0) {
+                        $order->add_product( $product, $cartProd['quantity'] );
+                    } else {
                         $status = 400;
                         $messages[] = $product->get_name() . ' sem estoque suficiente';
                     };
@@ -82,14 +91,21 @@ class Checkout {
                 } else {
                     $status = 400;
                     $messages[] = $cartProd['sku'] . ' não existe';
-                }
-            }
+                };
+            };
         };
 
-        if ($status != 200) {
-            return REST\error_response('mercado_solidario_checkout_cart_error', $messages);
+        if ($status == 200) {
+            $order->set_created_via( 'admin' );
+            $order->calculate_totals();
+            $order->set_status('completed', 'Mercado Solidario');
+            $order->add_order_note( 'Criado por '.$user->user_login );
+
+            $order_id = $order->save();
+
+            return REST\success_response( [ 'order' => $order_id ] );
         } else {
-            return REST\success_response(true);
+            return REST\error_response('mercado_solidario_checkout_cart_error', $messages);
         };
     }
 
