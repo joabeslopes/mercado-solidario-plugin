cdad1edb26080c327c00a86be9f1a05b1349837b
joabeslopes <joabeslopes.jsl@gmail.com>
2025-10-09 11:08:37 -0300

    remove resquícios do atum pois ele não apaga ao desativar

--- mercado-solidario.php
+++ mercado-solidario.php
@@ -28,3 +28,38 @@ require_once('vendor/autoload.php' );
 new Mercado_Solidario\Pages\Main_Page();
 
 new Mercado_Solidario\REST\Router();
+
+function remove_atum_roles(){
+    $admin = get_role('administrator');
+    if ($admin) {
+        foreach ($admin->capabilities as $cap => $has_cap) {
+            if (str_starts_with($cap, 'atum_')) {
+                $admin->remove_cap($cap);
+            }
+        }
+    }
+};
+
+function remove_atum_post_types() {
+    global $wpdb;
+
+    $post_types = $wpdb->get_col("
+        SELECT DISTINCT post_type 
+        FROM {$wpdb->posts}
+        WHERE post_type LIKE 'atum_%'
+    ");
+
+    foreach ($post_types as $post_type) {
+        $wpdb->query(
+            $wpdb->prepare("DELETE pm FROM {$wpdb->postmeta} pm
+                            LEFT JOIN {$wpdb->posts} p ON p.ID = pm.post_id
+                            WHERE p.ID IS NULL OR p.post_type = %s", $post_type)
+        );
+        $wpdb->query(
+            $wpdb->prepare("DELETE FROM {$wpdb->posts} WHERE post_type = %s", $post_type)
+        );
+    }
+};
+
+remove_atum_roles();
+remove_atum_post_types();
