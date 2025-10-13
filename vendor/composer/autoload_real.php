7097869df879a75848f5930b7dd0c4e2d2a87207
joabeslopes <joabeslopes.jsl@gmail.com>
2025-10-13 13:18:25 -0300

    biblioteca plugin-update-checker

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit6b7004cdcb9780badf4c1eba2c20c8aa
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit6b7004cdcb9780badf4c1eba2c20c8aa', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit6b7004cdcb9780badf4c1eba2c20c8aa', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit6b7004cdcb9780badf4c1eba2c20c8aa
         call_user_func(\Composer\Autoload\ComposerStaticInit6b7004cdcb9780badf4c1eba2c20c8aa::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit6b7004cdcb9780badf4c1eba2c20c8aa::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
