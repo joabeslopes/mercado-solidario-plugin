22145535db3c64b7178e14b3c062a25e2a99c8c4
joabeslopes <joabeslopes.jsl@gmail.com>
2026-01-11 23:42:41 -0300

    rascunho pagina fila de familias

--- frontend/src/components/FamilyQueue.vue
+++ frontend/src/components/FamilyQueue.vue
@@ -3,37 +3,15 @@ import '../css/global.css';
 import { post } from '../js/myApiClient';
 import { ref } from 'vue';
 import showPopup from '../js/myPopup';
-
+import Loading from './Loading.vue';
 import FamilySearch from './FamilySearch.vue';
 
 const loading = ref(false);
+const familyId = ref('');
 
-async function sendSearch(){
+async function addQueue(){
 
   loading.value = true;
-
-  const userCart = stockObj.getCart();
-
-//   if (userCart == null){
-//     showPopup("Erro", "Carrinho vazio");
-//     loading.value = false;
-//     return null;
-//   };
-
-//   const request = {
-//     'cart': userCart
-//   };
-
-//   const response = await post( '/checkout', request );
-
-//   if (response.status == 200) {
-//     showPopup("Sucesso", "Compra efetuada");
-//     stockObj.clearCart();
-//   } else {
-//     showPopup("Erro", response.message);
-//   };
-
-  loading.value = false;
 
 };
 
@@ -51,19 +29,3 @@ async function sendSearch(){
   </div>
 
 </template>
-
-<style scoped>
-
-@media (max-width: 768px) {
-  .cart {
-    order: 1;
-  }
-}
-
-@media (max-width: 768px) {
-  .stock {
-    order: 2;
-  }
-}
-
-</style>
