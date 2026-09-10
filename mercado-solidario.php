4bb0db2fffd138fcc0546c9ba6b95195191eb55e
joabeslopes <joabeslopes.jsl@gmail.com>
2026-09-10 19:32:15 -0300

    plugin update

--- mercado-solidario.php
+++ mercado-solidario.php
@@ -6,7 +6,7 @@ Text Domain: mercado-solidario
 Requires Plugins: woocommerce
 Description: Mercado solidario
 Author: Joabe Lopes
-Version: 1.6
+Version: 1.7
 Author URI: https://sitejoabe.mooo.com
 Requires at least:    6.0
 Tested up to:         6.9
